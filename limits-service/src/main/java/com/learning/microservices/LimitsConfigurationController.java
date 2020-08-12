2c60c8fcc7a098d8bdd22780f7246f4587009c4e
Vaibhav <vaibhavchaudhari918@gmail.com>
2020-08-12 21:53:45 +0530

    Configuration via application.proprties

--- limits-service/src/main/java/com/learning/microservices/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/learning/microservices/LimitsConfigurationController.java
@@ -1,15 +1,19 @@
 package com.learning.microservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.microservices.bean.LimitConfiguration;
+import com.learning.microservices.config.Configuration;
 
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration configuration;
 	@GetMapping("/limits")
 	public LimitConfiguration retriveLimitsFromConfigurations() {
-		return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
 	}
 }
